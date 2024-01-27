14f2bc36716f490bbdb188b14b73da7302c05ffb
DavideCiammetti <davideciammetti97@gmail.com>
2024-01-27 12:08:48 +0100

    fix: fine footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,169 @@
 <footer>
-    sono footer
+    <div class="foot-main-container position-relative d-flex flex-column">
+        <!-- info -->
+        <div class="d-flex">
+            <!-- lista 1 -->
+            <div class="list-container">
+                <!-- list elements -->
+                <div class="content-list">
+                    <ul>
+                        <li class="info-list">
+                            <h3>
+                                <strong>DC COMICS</strong>
+                            </h3>
+                        </li>
+                        <li class="info-list">
+                            <a href="#">Charter</a>
+                        </li>
+                        <li class="info-list">
+                            <a href="#">Comics</a>
+                        </li>
+                        <li class="info-list">
+                            <a href="#">Movie</a>
+                        </li>
+                        <li class="info-list">
+                            <a href="#">Tv</a>
+                        </li>
+                        <li class="info-list">
+                            <a href="#">Games</a>
+                        </li>
+                        <li class="info-list">
+                            <a href="#">Videos</a>
+                        </li>
+                        <li class="info-list">
+                            <a href="#">News</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- list elements -->
+                    <!-- list shop -->
+                <div class="content-list">
+                    <ul>
+                        <li class="info-list">
+                            <h3 class="shop-margin">
+                                <strong>SHOP</strong>
+                            </h3>
+                        </li>
+                        <li class="info-list">
+                            <a href="#">Shop DC</a>
+                        </li>
+                        <li class="info-list">
+                            <a href="#">Shop DC Collectibles</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- list shop -->
+            </div>
+            <!-- lista 1 -->
+            <!-- lista 2 -->
+            <div class="list-container"> 
+                <ul>
+                    <li class="info-list">
+                        <h3>
+                            <strong>DC</strong>
+                        </h3>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">Terms of Use</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">Privacy Policy (New)</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">Ad Choices</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">Advertising</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">Jobs</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">Subscriptions</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">Talent Workshop</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">CPSC Certicates</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">Ratings</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">Shop Helps</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- lista 2 -->
+            <!-- lista 3 -->
+            <div class="list-container">
+                <ul>
+                    <li class="info-list">
+                        <h3>
+                            <strong>SITES</strong>
+                        </h3>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">DC</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">MAD Magazine</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">DC Kids</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">DC Universe</a>
+                    </li>
+                    <li class="info-list">
+                        <a href="#">DC Power Vista</a>
+                    </li>
+                </ul>
+            <!-- lista 3 -->
+            <div class="dc-img-big position-absolute">
+                <img class="dc-img-big" src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="dc-logo-big.img">
+            </div>
+         <!-- info -->
+        </div>
+    </div>
+    <div class="subtitle-foot">
+        <p>
+            All Site Content Tm and <span><i class="fa-regular fa-copyright"></i></span>
+            2020 DC Entertinmet, uless otherwise <span><a class="text-decoration-none" href="#">noted hear</a></span>.
+            All rights reserved.
+        </p>
+        <p><a class="text-decoration-none" href="#">Cookie Setting</a></p>
+    </div>
+    </div>
+
+      {{-- footer menu --}}
+    <div class="foot-container d-flex justify-content-around align-items-center position-relative">
+        <div class="button-container">
+            <button>SIGN-UP NOW!</button>
+        </div>
+        <div class="d-flex align-items-center">
+            <p class="follow"><strong>FOLLOW US</strong></p>
+            <ul class="d-flex list-inline">
+                <li class="social-ico">
+                    <img class="dc-img-big" src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="facebook.img">
+                </li>
+                <li class="social-ico">
+                    <img class="dc-img-big" src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="twitter.img">
+                </li>
+                <li class="social-ico">
+                    <img class="dc-img-big" src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="youtube.img">
+                </li>
+                <li class="social-ico">
+                    <img class="dc-img-big" src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="pinterest.img">
+                </li>
+                <li class="social-ico">
+                    <img class="dc-img-big" src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="periscope.img">
+                </li>
+            </ul>
+        </div>
+    </div>
 </footer>
