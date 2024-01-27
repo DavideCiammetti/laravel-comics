84120f50552113c184f3d13c89602e2ad204a7f6
DavideCiammetti <davideciammetti97@gmail.com>
2024-01-27 13:08:12 +0100

    fix: correzione header, navbar e jumbotron

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,56 +1,60 @@
 <header id="header">
     <div class="blue-line">
       <div class="blue-second-line d-flex justify-content-end align-items-center">
-        <p class="fw-bolder register-visa"> DC POWER <span class="rm">RM</span>VISA <span><i class="fa-regular fa-registered"></i></span></p>
-        <p class="fw-bolder addition-dc-sites">ADDITIONAL DC SITES <span ><i class=" align-text-top fa-solid fa-sort-down"></i></span></p>
+        <p class="col-blue-300 fw-bolder register-visa"> DC POWER <span class="rm">RM</span>VISA <span><i class="fa-regular fa-registered"></i></span></p>
+        <p class="col-blue-300 fw-bolder addition-dc-sites">ADDITIONAL DC SITES <span ><i class=" align-text-top fa-solid fa-sort-down"></i></span></p>
       </div>
     </div>
 
-    {{-- menu container --}}
-    <div class="padding-18 d-flex justify-content-center align-items-center">
-        {{-- logo --}}
-        <div class="logo-img-container">
-            <img class="logo-img" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="img.dc-logo">
-        </div>
+    <nav>
         {{-- menu --}}
-        <nav>
-            <ul class="d-flex list-unstyled gap-5 text-decoration-none">
-                <li>
-                    <a class="fw-bolder" href="#">CHARTERS</a>
-                </li>
-                <li>
-                    <a class="fw-bolder" href="#">COMICS</a>
-                </li>
-                <li>
-                    <a class="fw-bolder" href="#">MOVIES</a>
-                </li>
-                <li>
-                    <a class="fw-bolder" href="">TV</a>
-                </li>
-                <li>
-                    <a class="fw-bolder" href="#">GAMES</a>
-                </li>
-                <li>
-                    <a class="fw-bolder" href="#">COLLECTIBLES</a>
-                    
-                </li>
-                <li>
-                    <a class="fw-bolder"  href="#">VIDEOS</a>
-                    
-                </li>
-                <li>
-                    <a class="fw-bolder" href="#">FANS</a>
-                    
-                </li>
-                <li>
-                    <a class="fw-bolder" href="#">NEWS</a>
-                    
-                </li>
-                <li>
-                    <a class="fw-bolder" href="#">SHOP<i class="shop-list align-text-top fa-solid fa-sort-down"></i></a>
-                </li>
-            </ul>
-        </nav>
-        @include('components.headerComponents.searchBar')
-    </div>
+        <ul class="d-flex list-unstyled gap-5 text-decoration-none justify-content-center">
+            <li>
+                {{-- logo --}}
+                <div class="logo-img-container">
+                    <img class="logo-img" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="img.dc-logo">
+                </div>
+            </li>
+            <li class="mt-5 border-bot-blue">
+                <a class="col-blue-300 fw-bolder" href="#">CHARTERS</a>
+            </li>
+            <li class="mt-5 border-bot-blue">
+                <a class="col-blue-300 fw-bolder" href="#">COMICS</a>
+            </li>
+            <li class="mt-5 border-bot-blue">
+                <a class="col-blue-300 fw-bolder" href="#">MOVIES</a>
+            </li>
+            <li class="mt-5 border-bot-blue">
+                <a class="col-blue-300 fw-bolder" href="">TV</a>
+            </li>
+            <li class="mt-5 border-bot-blue">
+                <a class="col-blue-300 fw-bolder" href="#">GAMES</a>
+            </li>
+            <li class="mt-5 border-bot-blue">
+                <a class="col-blue-300 fw-bolder" href="#">COLLECTIBLES</a>
+                
+            </li>
+            <li class="mt-5 border-bot-blue">
+                <a class="col-blue-300 fw-bolder"  href="#">VIDEOS</a>
+                
+            </li>
+            <li class="mt-5 border-bot-blue">
+                <a class="col-blue-300 fw-bolder" href="#">FANS</a>
+                
+            </li>
+            <li class="mt-5 border-bot-blue">
+                <a class="col-blue-300 fw-bolder" href="#">NEWS</a>
+                
+            </li>
+            <li class="mt-5 border-bot-blue">
+                <a class="col-blue-300 fw-bolder" href="#">SHOP<i class="shop-list align-text-top fa-solid fa-sort-down"></i></a>
+            </li>
+            <li>
+                @include('components.headerComponents.searchBar')
+            </li>
+        </ul>
+        <div class="jumbotron">
+
+        </div>
+    </nav>
 </header>
--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,7 +1,4 @@
 <main id="main">
-    <div class="jumbotron">
-
-    </div>
     <div class="position-relative grid-dc pt-5">
         <div class="position-but">
             <button type="button" class="text-light button-col border border-0 btn-primary">CURRENT SERIES</button>
